80dfbe1e41ee446b6e5e35c3a8b68f4b5ee3a658
lunapelchat <luna-marie.pelchat@anct.gouv.fr>
2024-03-20 10:37:38 +0100

    correction bug

--- src/app.js
+++ src/app.js
@@ -42,6 +42,45 @@ function breakLines(text, charactersPerLine) {
 // }
 
 
+// function operateurInfo(feature) {
+//     const operateurContent = document.getElementById("operateur-content");
+//     const dataInfos = feature.properties;
+//     operateurContent.classList.add("operator-content"); // permet de définir le style en CSS 
+//     const noOfferMessage = "Le territoire ne possède actuellement pas d'offre de nos partenaires.";
+
+//     // Nettoyer le contenu précédent
+//     operateurContent.innerHTML = '';
+
+//     if (dataInfos['opérateur1'] === noOfferMessage) {
+//         // Si 'opérateur1' a la valeur "noOfferMessage", afficher uniquement cette valeur
+//         const noOfferElement = document.createElement('div');
+//         noOfferElement.className = 'no-offer-message'; // Appliquer la classe CSS pour les boîtes en pointillés
+//         noOfferElement.textContent = noOfferMessage;
+//         operateurContent.appendChild(noOfferElement);
+//     } else {
+//         for (let i = 1; i <= 7; i++) {
+//             const operateurKey = 'opérateur' + i;
+//             const technologieKey = 'technologie' + i;
+//             const siteKey = 'site' + i;
+//             const priseEnChargeKey = 'prise_encharge' + i;
+//             const imgKey = 'Logo' + i;
+
+//             if (dataInfos[operateurKey]) {
+//                 // Utilisez innerHTML pour ajouter du contenu à l'élément
+//                 const infoElement = document.createElement('div');
+//                 infoElement.className = 'operator-separateur';
+//                 infoElement.innerHTML += '<img class="operator-logo" src="' + dataInfos[imgKey] + '" alt="Logo" style="max-width: 80px; max-height: 80px;">';
+//                 infoElement.innerHTML += '<br><b>Opérateur :</b> ' + dataInfos[operateurKey] + '<br>';
+//                 infoElement.innerHTML += '<b>Technologie :</b> ' + dataInfos[technologieKey] + '<br>';
+//                 infoElement.innerHTML += '<b>Site :</b> <a href="' + dataInfos[siteKey] + '" target="_blank">' + breakLines(dataInfos[siteKey], 40) + '</a><br>';
+//                 infoElement.innerHTML += '<b>Prise en charge supérieure à 300€ <br>(sous conditions de ressources) :</b> ' + dataInfos[priseEnChargeKey] + '<br>';
+
+//                 operateurContent.appendChild(infoElement);
+//             }
+//         }
+//     }
+// }
+
 function operateurInfo(feature) {
     const operateurContent = document.getElementById("operateur-content");
     const dataInfos = feature.properties;
@@ -58,6 +97,9 @@ function operateurInfo(feature) {
         noOfferElement.textContent = noOfferMessage;
         operateurContent.appendChild(noOfferElement);
     } else {
+        // Créer un tableau pour stocker les données des opérateurs
+        const operatorsData = [];
+
         for (let i = 1; i <= 7; i++) {
             const operateurKey = 'opérateur' + i;
             const technologieKey = 'technologie' + i;
@@ -66,42 +108,66 @@ function operateurInfo(feature) {
             const imgKey = 'Logo' + i;
 
             if (dataInfos[operateurKey]) {
-                // Utilisez innerHTML pour ajouter du contenu à l'élément
-                const infoElement = document.createElement('div');
-                infoElement.className = 'operator-separateur'; 
-                infoElement.innerHTML += '<img class="operator-logo" src="' + dataInfos[imgKey] + '" alt="Logo" style="max-width: 80px; max-height: 80px;">';
-                infoElement.innerHTML += '<br><b>Opérateur :</b> ' + dataInfos[operateurKey] + '<br>';
-                infoElement.innerHTML += '<b>Technologie :</b> ' + dataInfos[technologieKey] + '<br>';
-                infoElement.innerHTML += '<b>Site :</b> <a href="' + dataInfos[siteKey] + '" target="_blank">' + breakLines(dataInfos[siteKey], 40) + '</a><br>';
-                infoElement.innerHTML += '<b>Prise en charge supérieure à 300€ <br>(sous conditions de ressources) :</b> ' + dataInfos[priseEnChargeKey] + '<br>';
-
-                operateurContent.appendChild(infoElement);
+                // Ajouter les données de l'opérateur au tableau
+                operatorsData.push({
+                    operateur: dataInfos[operateurKey],
+                    technologie: dataInfos[technologieKey],
+                    site: dataInfos[siteKey],
+                    priseEnCharge: dataInfos[priseEnChargeKey],
+                    img: dataInfos[imgKey]
+                });
             }
         }
+
+        // Mélanger l'ordre des opérateurs
+        const shuffledOrder = shuffleArray([...Array(operatorsData.length).keys()]);
+
+        // Afficher les informations des opérateurs dans l'ordre mélangé
+        shuffledOrder.forEach(index => {
+            const operator = operatorsData[index];
+            const infoElement = document.createElement('div');
+            infoElement.className = 'operator-separateur'; 
+            infoElement.innerHTML += '<img class="operator-logo" src="' + operator.img + '" alt="Logo" style="max-width: 80px; max-height: 80px;">';
+            infoElement.innerHTML += '<br><b>Opérateur :</b> ' + operator.operateur + '<br>';
+            infoElement.innerHTML += '<b>Technologie :</b> ' + operator.technologie + '<br>';
+            infoElement.innerHTML += '<b>Site :</b> <a href="' + operator.site + '" target="_blank">' + breakLines(operator.site, 40) + '</a><br>';
+            infoElement.innerHTML += '<b>Prise en charge supérieure à 300€ <br>(sous conditions de ressources) :</b> ' + operator.priseEnCharge + '<br>';
+
+            operateurContent.appendChild(infoElement);
+        });
     }
+}
+
+// Fonction pour mélanger un tableau
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }
 
 
 // Création du contenu de la sidebar
 function createSidebarContent(feature) {
-    
+
     const cardHeader = document.querySelector(".card-header");
     const bntRetourAccueil = document.getElementById("bnt-retour-accueil");
     const operateurContent = document.getElementById("operateur-content");
     const dataInfos = feature.properties;
-    
+
     operateurContent.innerHTML = '';
-    
-  
+
+
     // Récupérer le titre : nom du département séléctionné
     cardHeader.innerHTML = '<div id="nom-programme" ><span><p class="dep-txt-sidebar">' + dataInfos[`dep`] + '</p></span></div>';
     // createListOperateur(feature);
     operateurInfo(feature);
-    bntRetourAccueil.style.display='block'
-    bntRetourAccueil.addEventListener('click', function(){
+    bntRetourAccueil.style.display = 'block'
+    bntRetourAccueil.addEventListener('click', function () {
         sidebar.open('home');
     });
-  
+
     return operateurContent;
 };
 
@@ -157,8 +223,10 @@ function createGeoJSON(data, map) {
                 mouseout: handleMouseOut
             });
         }
-        
-    }).addTo(map);
+
+
+    })//.bindTooltip(e => e.feature.properties.dep)//
+        .addTo(map);
 
     return layer;
 }
@@ -180,7 +248,7 @@ function createGeoJSONHabBis(data, map) {
         style: {
             color: '#F2E8DC', //beige 
             weight: 1,
-            fillOpacity:1,
+            fillOpacity: 1,
         },
     }).addTo(map);
 };
@@ -230,14 +298,14 @@ function resetDepStyles(depLayerResetStyle) {
 }
 
 //Creation de la barre de recherche
-function createSearchBar(data, map){
+function createSearchBar(data, map) {
     var searchControl = new L.Control.Search({
         layer: data,
         propertyName: 'dep',
         marker: false,
         textPlaceholder: 'Rechercher un département',
         position: 'topright',
-        collapsed : false,
+        collapsed: false,
         zoom: 12
     });
     //Au résultat, on zoom sur les limites du départements recherché puis on y applique le style du departement sélectionné
@@ -251,7 +319,7 @@ function createSearchBar(data, map){
             fillColor: '#e62d3C', //rouge
             fillOpacity: 1,
             weight: 1
-        });        
+        });
         selectedFeature.fire('click');
     });
     //Ajout barre de recherche
@@ -278,7 +346,7 @@ function calculateBounds() {
 /* --------------- */
 /*     CARTE      */
 /* -------------- */
-var mymap = L.map('map').setView([46.603354, 0.10000],5);
+var mymap = L.map('map').setView([46.603354, -18], 5);
 
 // Mise à jour de l'emprise maximale lors du redimensionnement de la fenêtre
 window.addEventListener('resize', function () {
@@ -320,19 +388,19 @@ const sidebar = L.control.sidebar({
 sidebar.on('content', function (ev) {
     switch (ev.id) {
         case 'home':
-          sidebar.options.autopan = true;
-          break;
+            sidebar.options.autopan = true;
+            break;
         case 'search-tab':
-          sidebar.options.autopan = true;
-          break;
+            sidebar.options.autopan = true;
+            break;
         case 'a-propos':
-        sidebar.options.autopan = true;
-        break;
-          default:
-          sidebar.options.autopan = true;
+            sidebar.options.autopan = true;
+            break;
+        default:
+            sidebar.options.autopan = true;
     }
 });
-  
+
 sidebar.addTo(mymap);
 sidebar.open('home');
 
@@ -344,8 +412,8 @@ sidebar.open('home');
 const fthdInit = loadData("data/geom/data_dep.geojson");
 // Ajout couches d'habillage
 const contoursIleReprojete = loadData("data/geom/contours_iles_reprojetees.geojson");
-const territoiresFrontaliers= loadData("data/geom/territoires_frontaliers.geojson");
-const habGuyane= loadData("data/geom/habillage_guyane.geojson");
+const territoiresFrontaliers = loadData("data/geom/territoires_frontaliers.geojson");
+const habGuyane = loadData("data/geom/habillage_guyane.geojson");
 
 // Création couches
 Promise.all([fthdInit, contoursIleReprojete, territoiresFrontaliers, habGuyane]).then(([fthdLayer, contoursIleReprojeteLayer, territoiresFrontaliersLayer, habGuyaneLayer]) => {
@@ -359,7 +427,7 @@ Promise.all([fthdInit, contoursIleReprojete, territoiresFrontaliers, habGuyane])
 
     // Barre de recherche
     createSearchBar(fthdData, mymap);
-    
+
 
     // Réinitialisation du style de département au clic sur un bouton de la sidebar
     const sidebarTabs = document.querySelector('.leaflet-sidebar-tabs');
